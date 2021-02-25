ca17e85b7a7adb54e18e554de945e910fb9d58fc
shc0801 <shc123987@naver.com>
2021-02-25 09:54:12 +0900

    2020-02-25 framework try1 15:27.18

---
+++ 2021-02-25 framework_1/src/App/Router.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+class Router {
+    static $pages = [];
+
+    static function __callStatic($name, $args) {
+        if(strtolower($_SERVER["REQUEST_METHOD"]) == $name) {
+            self::$page[] = $args;
+        }
+    }
+
+    static function start() {
+        $url = explode("?", $_SERVER["REQUEST_URI"])[0];
+
+        foreach(self::$pages as $page) {
+            if($url == $page[0]) {
+                $action = explode("@", $page[1]);
+                $conName = "Controller\\{$action[0]}";
+                $con = new $conName();
+                $con->{$action[1]}();
+                exit;
+            }
+        }
+    }
+}
---
+++ 2021-02-25 framework_1/src/helper.php
@@ -0,0 +1,53 @@
+<?php
+
+function go($url, $msg) {
+    echo "<script>";
+    echo "alert('$msg');";
+    echo "location.href='$url';";
+    echo "</script>";
+    exit;
+}
+
+function back($msg) {
+    echo "<script>";
+    echo "alert('$msg');";
+    echo "history.back();";
+    echo "</script>";
+    exit;
+}
+
+function view($name, $data = []) {
+    extract($data);
+    require VIEW."/header.php";
+    require VIEW."/$name.php";
+    require VIEW."/footer.php";
+    exit;
+}
+
+function user() {
+    return isset($_SESSION["user"]) ? $_SESSION["user"] : false;
+}
+
+function pagenation($data) {
+    define("LIST_LENGTH", 10);
+    define("BLOCK_LENGTH", 5);
+    define("SPACING", 4);
+
+    $page = isset($_GET["page"]) && is_numeric($_GET["page"]) && $_GET["page"] > 1 ? $_GET["page"] : 1;
+
+    $totalPage = ceil(count($data) / LIST_LENGTH);
+
+    $startPage = ceil($data * BLOCK_LENGTH) / BLOCK_LENGTH - SPACING;
+
+    $endPage = $startPage + SPACING > $totalPage ? $totalPage : $startPage + SPACING;
+
+    $next = true;
+    $prev = true;
+
+    if($startPage == 1) $prev = false;
+    if($endPage == $totalPage) $next = false;
+
+    $data = array_slice($data, ($page - 1) * LIST_LENGTH, LIST_LENGTH);
+
+    return (object)compact("data", "startPage", "endPage", "prev", "next", "page");
+}
